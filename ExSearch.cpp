68efcd70410ad1521eca0108f4fb5568a043f6bc
PengLei-Adam <ftgh2003@126.com>
2015-04-25 16:27:22 +0800

    ExtremumSearch

---
+++ ExSearch.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+
+#define PHI (-1.0+sqrt(5.0))/2.0		//define golden ratio
+
+typedef float (*PFN)(float x);
+
+float gss( PFN f, float x0, float x1, float crit ) {	//golden section search[x0,x1]
+	float x2 = x0 + (x1-x0)* PHI;
+	float x3;
+	while( fabs(x0 - x1) > crit ) {				//区间小于误差限
+		x3 = x0 + (x2-x0)* PHI;					//x3取距x0较远的黄金分割点
+		if( (*f)(x3) < (*f)(x2) )				
+		{		x2 = x3;  x1 = x2; }			//以[x0,x3,x2]为新区间
+		else
+		{		x0 = x1;  x1 = x3; }			//x0,x1大小关系颠倒,以[x3,x2,x1]为新区间
+	}
+	return (x0+x1)/2.0;
+}
+
+
